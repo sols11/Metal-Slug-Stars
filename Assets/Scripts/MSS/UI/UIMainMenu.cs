36f83aae0647cda48133d5b92625b78378a4a196
sols11 <caodahan97@126.com>
2017-10-30 10:01:01 +0800

    做完了单挑模式的雏形

---
+++ Assets/Scripts/MSS/UI/UIMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIMainMenu : MonoBehaviour
+{
+    public Button BtnChallenge;
+
+    private void Start()
+    {
+        BtnChallenge.onClick.AddListener(OnBtnChallengeClick);
+    }
+
+    private void OnBtnChallengeClick()
+    {
+        SceneManager.LoadScene("Map01");
+    }
+}
