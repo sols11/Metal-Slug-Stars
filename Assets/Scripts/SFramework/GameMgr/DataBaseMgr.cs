a7c9eb5efef7a70cabd85a38734f41577cc3063c
sols11 <caodahan97@126.com>
2017-10-31 15:04:01 +0800

    勉强把框架放进来了

---
+++ Assets/Scripts/SFramework/GameMgr/DataBaseMgr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SFramework
+{
+    /// <summary>
+    /// 游戏数据库
+    /// 存放游戏中的所有装备、道具数据
+    /// </summary>
+    public class DataBaseMgr : IGameMgr
+    {
+        // 字典存放所有种类的Item，key也可以用enum
+        public Dictionary<string, IEquip> dicEquip;
+        public Dictionary<string, IEquip> dicEnemyEquip;
+        public IProp[] Props { get; private set; }
+
+        public DataBaseMgr(GameMainProgram gameMain) : base(gameMain)
+        {
+        }
+
+        public override void Awake()
+        {
+            dicEquip = gameMain.fileMgr.LoadJsonDataBase<Dictionary<string, IEquip>>("Equip");
+            Props = gameMain.fileMgr.LoadJsonDataBase<IProp[]>("Prop");
+            dicEnemyEquip = gameMain.fileMgr.LoadJsonDataBase<Dictionary<string, IEquip>>("EnemyEquip");
+
+            //gameMain.fileMgr.CreateJsonDataBase("Tasks",Tasks);
+        }
+
+    }
+}
